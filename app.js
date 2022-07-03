7f01dd5923f9200f8ccb13f665df02824f1dbd3c
Roy Elman <airplanemodes@Roys-MacBook-Pro.local>
2022-07-03 20:39:55 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const server = http.createServer(app);
+
+let port = process.env.PORT || '3500';
+server.listen(port, () => console.log(`Listening on port ${port}...`));
